c532f34cadb4412f688a8c16ad5cde9f3e80b6f2
Kingsley Ani <kingsleyani4real@gmail.com>
2022-01-17 14:50:48 +0100

    second commit

--- loan calculator/src/com/kingsani/Main.java
+++ loan calculator/src/com/kingsani/Main.java
@@ -8,18 +8,36 @@ public class Main {
     public static void main(String[] args) {
 	final byte MONTHS_IN_YEAR = 12;
     final byte PERCENT = 100;
+    int principal = 0;
+        byte years = 0;
+        float monthlyInterest = 0;
+        int numberOfPayment = 0;
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Principal :  ");
-        int principal = scanner.nextInt();
-
-        System.out.print("Annual interest Rate :  ");
-        float annualInterest = scanner.nextFloat();
-        float monthlyInterest = annualInterest / PERCENT /MONTHS_IN_YEAR;
-
+        while(true) {
+            System.out.print("Principal :  ");
+            principal = scanner.nextInt();
+            if (principal >=1000 && principal <= 1_000_000)
+                break;
+            System.out.println("Enter a value between 1000 and 1000000");
+        }
+while (true) {
+    System.out.print("Annual interest Rate :  ");
+    float annualInterest = scanner.nextFloat();
+if (annualInterest >=1 && annualInterest <=30) {
+    monthlyInterest = annualInterest / PERCENT / MONTHS_IN_YEAR;
+break;
+}
+    System.out.println("Enter a value between 1 and 30");
+}
+    while (true) {
         System.out.print("Period (years) :  ");
-        byte years = scanner.nextByte();
-        int numberOfPayment = years * MONTHS_IN_YEAR;
-
+        years = scanner.nextByte();
+        if(years >= 1 && years <=30) {
+            numberOfPayment = years * MONTHS_IN_YEAR;
+            break;
+        }
+        System.out.println("Enter a value between 1 and 30.");
+    }
         Double mortgage = principal * (monthlyInterest* Math.pow(1 + monthlyInterest,numberOfPayment)/
                 Math.pow(1 + monthlyInterest,numberOfPayment) - 1);
         String mortgageFormatted = NumberFormat.getCurrencyInstance().format(mortgage);
