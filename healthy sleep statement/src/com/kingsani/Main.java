31c3f3f7293610c575c331e0459700bd1f80a966
Kingsley Ani <kingsleyani4real@gmail.com>
2022-01-19 16:00:20 +0100

    first commit

---
+++ healthy sleep statement/src/com/kingsani/Main.java
@@ -0,0 +1,22 @@
+package com.kingsani;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int h = scanner.nextInt();
+        if (h < a) {
+            System.out.println("Deficiency");
+        } else if (h > b) {
+            System.out.println("Excess");
+        } else {
+            System.out.println("Normal");
+        }
+    }
+}
+
+
